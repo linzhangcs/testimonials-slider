78127ef75c0362645bcb558da3e70914fddb61a0
Lin Zhang <lin.zhang.ux@gmail.com>
2020-07-14 00:40:12 -0700

    adjust style for changing quote and title

--- javascript/main.js
+++ javascript/main.js
@@ -1,8 +1,7 @@
 (function(){
     const SLIDEIMGS = ['../images/image-tanya.jpg', '../images/image-john.jpg'];
-    const SLIDETEXTS = [`“ I’ve been interested in coding for a while but never taken the jump, until now. 
-    I couldn’t recommend this course enough. I’m now in the job of my dreams and so 
-    excited about the future. ”`, `“ If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer. ”`];
+    const SLIDETEXTS = [`“ I’ve been interested in coding for a while but never taken the jump, until now. I couldn’t recommend this course enough. I’m now in the job of my dreams and so excited about the future. ”`, 
+    `“ If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer. ”`];
     const NAMES = ['Tanya Sinclair', 'John Tarkpor'];
     const TITLES = ['UX Engineer', 'Junior Front-end Developer'];
 
@@ -11,14 +10,11 @@
     const IMAGE = document.querySelector('.image');
     const QUOTE = document.querySelector('.testimonial-text');
     const NAME = document.querySelector('.testimonial-name');
-    // const TITLE = document.querySelector('.testimonial-title');
 
     LEFT.addEventListener('click', event =>{
-        console.log("left arrow clicked");
         showSlide(-1);
     });
     RIGHT.addEventListener('click', event => {
-        console.log("right arrow clicked");
         showSlide(1);
     })
 
